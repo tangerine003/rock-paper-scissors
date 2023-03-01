55c1bb9590726f8067160796dff49c073e5b988f
tangerine003 <123626883+tangerine003@users.noreply.github.com>
2023-03-01 15:53:38 +0530

    Implement first to five wins approach instead of previous best of five

--- javascript.js
+++ javascript.js
@@ -35,113 +35,125 @@ let computerScore = 0,
   - if one player chooses paper and the other user chooses scissors, then the player who chose scissors get a point.
   - if both the players have the same choice then no one gets a point. */
 function playRound(playerSelection, computerSelection) {
-  computerSelection = getComputerChoice();
-  playerSelection = prompt("User , please enter your selection");
-  console.log(computerSelection);
-  console.log(playerSelection);
-
-  //both players choose same value
-  if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-    console.log(
-      `It's a tie. Score is Computer: ${computerScore} and Player: ${playerScore} `
-    );
-  }
-
-  //one player chooses rock and the other chooses scissors ,the one who chose rock will win
-  else if (
-    (playerSelection.toUpperCase() == "ROCK" ||
-      playerSelection.toUpperCase() == "SCISSORS") &&
-    (computerSelection.toUpperCase() == "ROCK" ||
-      computerSelection.toUpperCase() == "SCISSORS")
-  ) {
-    if (playerSelection.toUpperCase() == "ROCK") {
-      console.log("Player won this round.");
-      ++playerScore;
-    } else {
-      console.log("Computer won this round");
-      ++computerScore;
+  if (!(playerScore == 5 || computerScore == 5)) {
+    computerSelection = getComputerChoice();
+    //playerSelection = prompt("User , please enter your selection");
+    if (this.classList.value == "rock") {
+      playerSelection = "rock";
     }
-    console.log(` Score is Computer: ${computerScore} and Player: ${playerScore} `);
-  }
-
-  //one player chooses rock and the other  chooses paper ,the one who chose paper will win
-  else if (
-    (playerSelection.toUpperCase() == "ROCK" ||
-      playerSelection.toUpperCase() == "PAPER") &&
-    (computerSelection.toUpperCase() == "ROCK" ||
-      computerSelection.toUpperCase() == "PAPER")
-  ) {
-    if (playerSelection.toUpperCase() == "PAPER") {
-      console.log("Player won this round.");
-      ++playerScore;
-    } else {
-      console.log("Computer won this round");
-      ++computerScore;
+    if (this.classList.value == "paper") {
+      playerSelection = "paper";
     }
-    console.log(` Score is Computer: ${computerScore} and Player: ${playerScore} `);
-  }
-
-  //one player chooses paper and the other  chooses scissors ,the one who chose scissors will win
-  else if (
-    (playerSelection.toUpperCase() == "SCISSORS" ||
-      playerSelection.toUpperCase() == "PAPER") &&
-    (computerSelection.toUpperCase() == "SCISSORS" ||
-      computerSelection.toUpperCase() == "PAPER")
-  ) {
-    if (playerSelection.toUpperCase() == "SCISSORS") {
-      console.log("Player won this round.");
-      ++playerScore;
-    } else {
-      console.log("Computer won this round");
-      ++computerScore;
+    if (this.classList.value == "scissors") {
+      playerSelection = "scissors";
     }
-    console.log(` Score is Computer: ${computerScore} and Player: ${playerScore} `);
+    /*  div.textContent =
+    "computer's choice is " +
+    computerSelection +
+    "\n" +
+    "Your choice is " +
+    playerSelection;
+ */
+    container.textContent = `Computer's choice is ${computerSelection}.
+Your choice is ${playerSelection}`;
+    //both players choose same value
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+      conclusion.textContent = "It's a tie.";
+      container.appendChild(conclusion);
+      score.textContent = `Score is Computer: ${computerScore} and Player: ${playerScore}`;
+      container.appendChild(score);
+    }
+
+    //one player chooses rock and the other chooses scissors ,the one who chose rock will win
+    else if (
+      (playerSelection.toUpperCase() == "ROCK" ||
+        playerSelection.toUpperCase() == "SCISSORS") &&
+      (computerSelection.toUpperCase() == "ROCK" ||
+        computerSelection.toUpperCase() == "SCISSORS")
+    ) {
+      if (playerSelection.toUpperCase() == "ROCK") {
+        conclusion.textContent = "Player won this round.";
+        container.appendChild(conclusion);
+        ++playerScore;
+      } else {
+        conclusion.textContent = "Computer won this round";
+        container.appendChild(conclusion);
+        ++computerScore;
+      }
+      score.textContent = `Score is Computer: ${computerScore} and Player: ${playerScore}`;
+      container.appendChild(score);
+    }
+
+    //one player chooses rock and the other  chooses paper ,the one who chose paper will win
+    else if (
+      (playerSelection.toUpperCase() == "ROCK" ||
+        playerSelection.toUpperCase() == "PAPER") &&
+      (computerSelection.toUpperCase() == "ROCK" ||
+        computerSelection.toUpperCase() == "PAPER")
+    ) {
+      if (playerSelection.toUpperCase() == "PAPER") {
+        conclusion.textContent = "Player won this round.";
+        container.appendChild(conclusion);
+        ++playerScore;
+      } else {
+        conclusion.textContent = "Computer won this round";
+        container.appendChild(conclusion);
+        ++computerScore;
+      }
+      score.textContent = `Score is Computer: ${computerScore} and Player: ${playerScore}`;
+      container.appendChild(score);
+    }
+
+    //one player chooses paper and the other  chooses scissors ,the one who chose scissors will win
+    else if (
+      (playerSelection.toUpperCase() == "SCISSORS" ||
+        playerSelection.toUpperCase() == "PAPER") &&
+      (computerSelection.toUpperCase() == "SCISSORS" ||
+        computerSelection.toUpperCase() == "PAPER")
+    ) {
+      if (playerSelection.toUpperCase() == "SCISSORS") {
+        conclusion.textContent = "Player won this round.";
+        container.appendChild(conclusion);
+        ++playerScore;
+      } else {
+        conclusion.textContent = "Computer won this round";
+        container.appendChild(conclusion);
+        ++computerScore;
+      }
+      score.textContent = `Score is Computer: ${computerScore} and Player: ${playerScore}`;
+      container.appendChild(score);
+    }
+  } else if (playerScore == 5) {
+    finalResult.textContent = "You won!! Computer lost!! ";
+    container.appendChild(finalResult);
+  } else {
+    finalResult.textContent = "Computer won!! You lost!! ";
+    container.appendChild(finalResult);
   }
 }
 
 /* this game is best of 5 so the player with highest points at the end of 5 rounds will win , and if there is a tie then there will be additional rounds until the tie is broken or until the user wishes to not proceed any further. */
 //main game function
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-  }
-  if (computerScore > playerScore) {
-    console.log("Computer Won the game");
-  } else if (computerScore == playerScore) {
-    console.log("It's a tie !");
-    let tieBreakerPrompt = prompt(
-      "Would you like to play another round to break the tie?"
-    );
-    if (tieBreakerPrompt.toUpperCase() == "YES") {
-      playToBreakTie();
-    } else {
-      console.log("Just deal with the game ending in a tie then.");
-    }
-  } else {
-    console.log("Player Won the game");
-  }
-}
+/* function playGame() {
+  playRound();
+} */
 
 //tie breaking function if a tie occurs at the end of 5 rounds.
-function playToBreakTie() {
-  playRound();
-  if (computerScore == playerScore) {
-    let tieBreakerPrompt = prompt(
-      "IT'S A TIE AGAIN !! Would you like to play another round to break the tie?"
-    );
-    if (tieBreakerPrompt.toUpperCase() == "YES") {
-      playToBreakTie();
-    } else {
-      console.log("Just deal with the game ending in a tie then.");
-    }
-  } else {
-    if (computerScore > playerScore) {
-      console.log("Computer Won the game");
-    } else {
-      console.log("Player Won the game");
-    }
-    console.log(`Score is Computer: ${computerScore} and Player: ${playerScore} `);
-  }
-}
 
-playGame();
+//playGame();
+
+const buttons = document.querySelectorAll("button");
+const container = document.querySelector(".container");
+//const div = document.querySelector("div.result");
+
+const conclusion = document.createElement("div");
+conclusion.classList.add("conclusion");
+
+const score = document.createElement("div");
+score.classList.add("score");
+
+const finalResult = document.createElement("div");
+finalResult.classList.add("final-result");
+buttons.forEach((button) => {
+  button.addEventListener("click", playRound);
+});
